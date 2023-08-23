b506de452695462dc92188b134b556cbcc97a902
daniel <dd482@njit.edu>
2023-08-22 21:30:28 -0400

    Updates from original repo

--- WebDev/app/server/services/render.js
+++ WebDev/app/server/services/render.js
@@ -4,12 +4,11 @@ exports.homeRoutes = (req,res)=>{
 
 exports.adminRoutes = (req,res)=>{
     const UserAgent = req.header('User-Agent')
-    if(UserAgent != "Skynet"){
-        //res.redirect('/');
+    /*if(UserAgent != "Skynet"){
         res.render('404')
     } else {
         res.render('admin')
-    }
+    }*/
     res.render('admin')
 }
 
